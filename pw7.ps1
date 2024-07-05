ef57b7b9f2cafd4560c677add95005b79bc0b8bf
Pancracio <38768857+Chapelo@users.noreply.github.com>
2024-07-06 01:44:42 +0200

    Update pw7.ps1

--- pw7.ps1
+++ pw7.ps1
@@ -1,1 +1,1 @@
-powershell -w h -NoP -Ep Bypass; Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.3/PowerShell-7.4.3-win-x64.msi" -OutFile "$env:USERPROFILE\Downloads\pw7.msi"; Start-Process -FilePath "$env:USERPROFILE\Downloads\pw7.msi"
+powershell -w h -NoP -Ep Bypass; Start-Process msiexec.exe -ArgumentList '/package PowerShell-7.4.3-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1' -NoNewWindow -Wait
