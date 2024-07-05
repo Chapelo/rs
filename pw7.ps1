fbd477b840755845e901e889a8afc71807b8f078
Pancracio <38768857+Chapelo@users.noreply.github.com>
2024-07-06 00:35:12 +0200

    Update pw7.ps1

--- pw7.ps1
+++ pw7.ps1
@@ -1,1 +1,1 @@
-powershell -w h Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.3/PowerShell-7.4.3-win-x64.msi" -OutFile "$env:USERPROFILE\Downloads\pw7.msi"; Start-Process -FilePath "$env:USERPROFILE\Downloads\pw7.msi"
+powershell -w h -NoP -Ep Bypass; Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.3/PowerShell-7.4.3-win-x64.msi" -OutFile "$env:USERPROFILE\Downloads\pw7.msi"; Start-Process -FilePath "$env:USERPROFILE\Downloads\pw7.msi"
